80f4e612f721edf69d97957ac974f5c52b3fb63f
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-13 15:31:46 +0200

    datamanclient

---
+++ Sources/Vaporized/MicroServices/Dataman/dataman-client.swift
@@ -0,0 +1,25 @@
+import Surfaces
+import Vapor
+
+public struct DatamanClient {
+    public let baseURL: URI
+    private let client: Client
+
+    public init(baseURL: URI, client: Client) {
+        self.baseURL = baseURL
+        self.client = client
+    }
+
+    public func send(
+        _ datamanRequest: DatamanRequest,
+        on req: Request
+    ) async throws -> DatamanResponse {
+        let response = try await client.post(baseURL) { post in
+            try post.content.encode(datamanRequest, as: .json)
+        }
+        guard (200..<300).contains(response.status.code) else {
+            throw Abort(.badGateway, reason: "Dataman returned \(response.status)")
+        }
+        return try response.content.decode(DatamanResponse.self)
+    }
+}
