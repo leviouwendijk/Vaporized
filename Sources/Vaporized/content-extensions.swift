4e266fd31e0af23ac9c6af39f69f372248466bc8
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-13 15:11:14 +0200

    content-ext

---
+++ Sources/Vaporized/content-extensions.swift
@@ -0,0 +1,12 @@
+import Surfaces
+import Vapor
+
+// Dataman
+extension DatamanRequest: @retroactive Content { }
+extension DatamanResponse: @retroactive Content { }
+
+// Captcher
+extension CaptcherTokenType: @retroactive Content {}
+extension CaptcherOperation: @retroactive Content {}
+extension CaptcherRequest: @retroactive Content {}
+extension CaptcherResponse: @retroactive Content {}
