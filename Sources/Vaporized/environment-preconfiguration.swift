4f4ff02f66a71d6689eef9205438ca482df33825
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-16 17:28:11 +0200

    environment preconfiguration

---
+++ Sources/Vaporized/environment-preconfiguration.swift
@@ -0,0 +1,59 @@
+import Foundation
+import Vapor
+
+public protocol EnvironmentPreconfigurationKeyProtocol: RawRepresentable, CaseIterable, Sendable, Hashable where RawValue == String {}
+
+private struct EnvironmentPreconfigurationKey<K>: StorageKey
+
+where K: EnvironmentPreconfigurationKeyProtocol {
+    typealias Value = EnvironmentPreconfiguration<K>
+}
+
+public struct EnvironmentPreconfiguration<K>: Sendable
+where K: EnvironmentPreconfigurationKeyProtocol
+{
+    private let storage: [K: String]
+
+    public init(keys: [K]) throws {
+        var dict = [K: String]()
+        for key in keys {
+            guard let val = Environment.get(key.rawValue), !val.isEmpty else {
+                throw Abort(.internalServerError, reason: "Missing \(key.rawValue)")
+            }
+            dict[key] = val
+        }
+        self.storage = dict
+    }
+
+    public init(app: Application, keys: [K]) throws {
+        let cfg = try Self(keys: keys)
+        app.storage[EnvironmentPreconfigurationKey<K>.self] = cfg
+        self = cfg
+    }
+
+    public func string(_ key: K) -> String {
+        storage[key]!
+    }
+}
+
+extension Application {
+    public func environmentPreconfiguration<K>(
+        _ type: K.Type
+    ) -> EnvironmentPreconfiguration<K>
+    where K: EnvironmentPreconfigurationKeyProtocol
+    {
+        guard let cfg = storage[EnvironmentPreconfigurationKey<K>.self] else {
+            fatalError("""
+                EnvironmentPreconfiguration<\(K.self)> not initialized; \
+                make sure you called `try EnvironmentPreconfiguration(app:…, keys: …)` in configure(_:)`
+            """)
+        }
+        return cfg
+    }
+}
+
+// example implementation:
+// public enum EnvironmentKeyObject: String, CaseIterable, Sendable, EnvironmentPreconfigurationKeyProtocol {
+//     case myAPIKey  = "MY_API_KEY"
+//     case someToken  = "EXAMPLE_TOKEN"
+// }
